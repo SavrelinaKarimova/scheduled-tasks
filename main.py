b1704033adb1f361c3d91f43cee8e62db1bf0330
SavrelinaKarimova <savrelinakarimova19@gmail.com>
2026-06-01 16:50:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import os
 from twilio.rest import Client
 
 OWM_Endpoint = 'https://api.openweathermap.org/data/2.5/forecast'
-api_key = os.environ.get("API_KEY")
-account_sid = os.environ.get("ACC_ID")
+api_key = os.environ.get("OWM_API_KEY")
+account_sid = os.environ.get("ACCOUNT_ID")
 auth_token = os.environ.get("AUTH_TOKEN")
 
 parameters = {
